30d9680c52b3622e61fb5b0ee6cca9deedbec9fe
SUMAN SAURABH <110984246+Git-suman309@users.noreply.github.com>
2023-03-30 18:13:24 +0530

    blog ass

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,39 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Blog blog = blogRepository2.findById(blogId).get();
+
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+
+        blog.getImageList().add(image);
+
+        blogRepository2.save(blog);
+
+        return image;
 
     }
 
     public void deleteImage(Integer id){
+        imageRepository2.deleteById(id);
 
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image=imageRepository2.findById(id).get();
 
+        String imageDim[]=image.getDimensions().split("X");
+        String screenDim[]=screenDimensions.split("X");
+
+        int imageDimensionhori=Integer.parseInt(imageDim[0]);
+        int imageDimensionvert=Integer.parseInt(imageDim[1]);
+        int screenDimensionhori=Integer.parseInt(screenDim[0]);
+        int screenDimensionveri=Integer.parseInt(screenDim[1]);
+        int count=(screenDimensionhori/imageDimensionhori)*(screenDimensionveri/imageDimensionvert);
+
+        return count;
     }
 }
